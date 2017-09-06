76072b6708d4bb8736ed4890006228a8fb820dad
Nick G <nickwgonzales@gmail.com>
2017-09-05 20:41:17 -0500

    request and data pulled

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 GENERATES REPOSITORY REPORTS
 This is for a personal pet project.
+
+Problem statement:
+
+Given a list of github repos, perform the following steps:
+
+Given a user/repo combo, pull data for the following (once as a GitHub API, and once with a web scraper):
+	-Commit history
+	-Issues
+	-Contributors
+	-Owner
+
+Format the data into a easily readable report.
+
+Make it a single button click with no dependencies (IE no nodejs).
---
+++ generate.js
@@ -0,0 +1,18 @@
+/*
+	Problem description:
+	Generate formatted HTML output for all passed data
+*/
+
+
+const request = require("./request.js");
+const fs = require("fs");
+
+let repos = fs.readFileSync("./repos.csv","UTF-8").split("\n").splice(1,2);
+repos.forEach(function(elt,i){
+	let a = elt.length-1;
+	repos[i] = elt.split("").splice(0,a).join("").split(",");
+	let repoIndex = repos[i][1].lastIndexOf("/") + 1;
+	let b = repos[i][1].length - 1;
+	repos[i][1] = repos[i][1].split("").splice(repoIndex,b).join("");
+});
+
